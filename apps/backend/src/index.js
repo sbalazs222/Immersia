e190e75527d356086e4b61c7c2c3abc39f8e1b0e
Zal-n <katonazalan816@gmail.com>
2026-01-02 18:35:30 +0100

    installed packages: cookie parser for cookie handling, psgutil for logging, cors.

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import cp from 'cookie-parser'; 
+import cors from 'cors';
 import { env } from './config.js';
-import { sharedHello } from '@repo/shared';
+import {colorLog, errorLog} from 'psgutil';
 
 const app = express();
+const corsOptions = {
+    credentials: true,
+    origin: config.FRONTEND_URL || `http://localhost:${env.PORT}`,
+}
 
-app.get('/', (_, res) => res.send(sharedHello()));
 
+app.use(cp());
+app.use(cors(corsOptions));
+app.use(colorLog);
+app.use(express.json());
+
+
+
+app.use(errorLog);
 app.listen(env.PORT, () => {
   console.log(`Backend running on http://localhost:${env.PORT}`);
 });
