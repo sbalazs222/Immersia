4ac308c18a6f7c89cadd3c836d524cdd9575ee2d
Zal-n <katonazalan816@gmail.com>
2026-01-03 21:53:15 +0100

    Login logic

--- apps/backend/src/controllers/authController.js
+++ apps/backend/src/controllers/authController.js
@@ -1,15 +1,45 @@
-import { req, res } from 'express';
+import pool from '../config/mysql.js';
+import argon2 from 'argon2';
+import { env } from '../config/config.js';
+import { generateAccessToken, generateRefreshToken } from '../utils/jwt.js';
 
-export function Login(req: Request, res: Response, next) {
+export async function Login(req, res, next) {
+  const conn = await pool.getConnection();
+  const { email, password } = req.body;
   try {
-    return res.status(200).json({
-      message: 'Successful login.', data: {
-        username: 'user',
-      }
+    const [user] = await conn.query('SELECT id, password FROM users WHERE email = ?', [email]);
+    if (user.length < 1) return res.status(400).json({ message: 'Invalid username or password' });
+
+    if (!(await argon2.verify(user[0].password, password)))
+      return res.status(400).json({ message: 'Invalid username or password' });
+
+    const payload = {
+      id: user[0].id,
+      user: user[0].email,
+      role: user[0].role,
+    };
+
+    const accessToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 15 * 60 * 1000, // 15 minutes
     });
-  }
-  catch (error) {
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return res.status(200).json({ message: 'Successful login.' });
+  } catch (error) {
     next(error);
+  } finally {
+    conn.release();
   }
 }
 export function Register(req, res, next) {
@@ -17,20 +47,13 @@ export function Register(req, res, next) {
     const body = req.body;
 
     return res.status(200).json({
-      message: 'Successful registration.', data:
-        JSON.stringify(body),
+      message: 'Successful registration.',
+      data: JSON.stringify(body),
     });
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
   }
 }
-export function Verify(req, res, next) {
-
-}
-export function Refresh(req, res, next) {
-
-}
-export function Logout(req, res, next) {
-
-}
+export function Verify(req, res, next) {}
+export function Refresh(req, res, next) {}
+export function Logout(req, res, next) {}
