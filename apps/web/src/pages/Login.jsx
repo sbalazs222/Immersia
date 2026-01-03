93fdc0e563158df1da0a50b8a05b65c79746ee87
sbalazs222 <sbalazs222@gmail.com>
2026-01-03 15:42:43 +0100

    added login logic, added .env

---
+++ apps/web/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+export default function Login() {
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const formdata = new FormData(event.target)
+        const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: formdata.get('email'),
+                password: formdata.get('password')
+            })
+        })
+        if (res.status === 200) {
+            alert('Login successful')
+        }
+    }
+    return (
+        <>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email"/>
+                <input type="password" name="password"/>
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
+}
