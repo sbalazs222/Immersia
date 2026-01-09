be1c658bde673a7b577950bc836a257cd7673c19
Zal-n <katonazalan816@gmail.com>
2026-01-09 21:17:15 +0100

    added required field validation TODO: move to utils

--- apps/backend/src/routes/authRoutes.js
+++ apps/backend/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { Login, Logout, Refresh, Register, Verify } from '../controllers/authController.js';
 import { authenticateToken } from '../middlewares/authentication.js';
+import { validateRequiredFields } from 'psgutil';
 
 const AuthRouter = Router();
 
-AuthRouter.post('/login', Login);
-AuthRouter.post('/register', Register);
+AuthRouter.post('/login', validateRequiredFields(['email', 'password']), Login);
+AuthRouter.post('/register', validateRequiredFields(['email', 'password']), Register);
 AuthRouter.post('/refresh', Refresh);
 AuthRouter.post('/verify', authenticateToken, Verify);
 AuthRouter.post('/logout', authenticateToken, Logout);
