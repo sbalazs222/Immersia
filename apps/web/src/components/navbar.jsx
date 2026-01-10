b4000b6c5e62183ac52745c6eea1387eeb99eef5
sbalazs222 <sbalazs222@gmail.com>
2026-01-10 17:24:36 +0100

    added logout logic and button

--- apps/web/src/components/navbar.jsx
+++ apps/web/src/components/navbar.jsx
@@ -1,17 +1,36 @@
 import { NavLink, Route, Routes, BrowserRouter } from 'react-router-dom'
-import { Nav } from 'react-bootstrap'
+import { Nav, Button } from 'react-bootstrap'
+import { toast } from 'react-toastify'
 
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 
 export default function Navbar({ isLoggedIn, setIsLoggedIn }) {
+
+    async function handleLogout() {
+        const res = await fetch(`/auth/logout`, {
+            method: 'POST',
+            credentials: 'include',
+        });
+        if (res.ok) {
+            toast.success('Logged out successfully')
+            setIsLoggedIn(false)
+        }
+        else {
+            toast.error('Internal server error, ' + await res.json().message)
+        }
+    }
+
     return (
         <BrowserRouter>
             <nav>
                 {
                     isLoggedIn ? 
                     (
-                        <Nav.Link as={NavLink} to="/">Scenes</Nav.Link>
+                        <>
+                            <Nav.Link as={NavLink} to="/">Scenes</Nav.Link>
+                            <Button onClick={handleLogout}>Logout</Button>
+                        </>
                     )
                     :
                     (
