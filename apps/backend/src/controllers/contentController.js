b9ae49c713402963d5edd69a1f2adc2b3cb6c455
Zal-n <katonazalan816@gmail.com>
2026-01-10 15:01:44 +0100

    jsdoc + error handling

--- apps/backend/src/controllers/contentController.js
+++ apps/backend/src/controllers/contentController.js
@@ -1,6 +1,14 @@
 const CATEGORIES = ['oneshot', 'ambience', 'scene'];
 import pool from '../config/mysql.js';
 
+/**
+ * Handles serving data to frontend
+ * @param {import('express').Request} req - Express request object.
+ * @param {import('express').Response} res - Express response object.
+ * @param {import('express').NextFunction} next - Express next function for error handling.
+ * * @returns {Promise<void>} Returns a 200 JSON response on success.
+ * * @throws {Error} Passes any internal server error to the `next` middleware.
+ */
 export async function ServeData(req, res, next) {
   const conn = await pool.getConnection();
 
@@ -36,13 +44,21 @@ export async function ServeData(req, res, next) {
     if (conn) conn.release();
   }
 }
-
+/**
+ * Handles serving data about a single audio file to frontend
+ * @param {import('express').Request} req - Express request object.
+ * @param {import('express').Response} res - Express response object.
+ * @param {import('express').NextFunction} next - Express next function for error handling.
+ * * @returns {Promise<void>} Returns a 200 JSON response on success, 404 when sound file does not exists.
+ * * @throws {Error} Passes any internal server error to the `next` middleware.
+ */
 export async function GetSoundData(req, res, next) {
   const conn = await pool.getConnection();
   try {
     const slug = req.params.slug;
 
     const [result] = await conn.query('SELECT * FROM sounds WHERE slug = ?', [slug]);
+    if (result < 1) return res.sendStatus(404);
 
     return res.status(200).json(result[0]);
   } catch (error) {
@@ -51,23 +67,38 @@ export async function GetSoundData(req, res, next) {
     if (conn) conn.release();
   }
 }
+/**
+ * Handles providing audio files using nginx
+ * @param {import('express').Request} req - Express request object.
+ * @param {import('express').Response} res - Express response object.
+ * @param {import('express').NextFunction} next - Express next function for error handling.
+ * * @returns {Promise<void>} Returns a 200 JSON response on success, 404 when sound file does not exists.
+ * * @throws {Error} Passes any internal server error to the `next` middleware.
+ */
+export async function PlaySound(req, res, next) {
+  try {
+    const slug = req.params.slug;
 
-export async function PlaySound(req, res) {
-  const slug = req.params.slug;
+    const [file] = await pool.query('SELECT sound_file_path FROM sounds WHERE slug = ?', [slug]);
 
-  const [file] = await pool.query('SELECT sound_file_path FROM sounds WHERE slug = ?', [slug]);
-
-  res.set('X-Accel-Redirect', '/internal/' + file[0].sound_file_path);
-  res.set('Content-Type', 'audio/mpeg');
-  res.end();
+    res.set('X-Accel-Redirect', '/internal/' + file[0].sound_file_path);
+    res.set('Content-Type', 'audio/mpeg');
+    res.end();
+  } catch (error) {
+    next(error);
+  }
 }
 
-export async function GetThumbnail(req, res) {
-  const slug = req.params.slug;
+export async function GetThumbnail(req, res, next) {
+  try {
+    const slug = req.params.slug;
 
-  const [file] = await pool.query('SELECT image_file_path FROM sounds WHERE slug = ?', [slug]);
+    const [file] = await pool.query('SELECT image_file_path FROM sounds WHERE slug = ?', [slug]);
 
-  res.set('X-Accel-Redirect', '/internal/' + file[0].image_file_path);
-  res.set('Content-Type', 'image/jpeg');
-  res.end();
+    res.set('X-Accel-Redirect', '/internal/' + file[0].image_file_path);
+    res.set('Content-Type', 'image/jpeg');
+    res.end();
+  } catch (error) {
+    next(error);
+  }
 }
