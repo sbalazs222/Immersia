746094f66546a903c16422f2d476f5537e298f9a
Zal-n <katonazalan816@gmail.com>
2026-02-07 20:43:25 +0100

    functions

--- apps/mock-backend/src/index.js
+++ apps/mock-backend/src/index.js
@@ -4,18 +4,19 @@ import cors from 'cors';
 import { colorLog, errorLog, validateFieldCount, validateRequiredFields } from 'psgutil';
 import { ConsoleColor as cc } from 'psgutil';
 import cookieParser from 'cookie-parser';
-import { Login } from './functions.js';
+import { Login, authenticateToken, Register, Refresh, Verify, Logout, All, Sound, Play, Thumb, NewSound, NewArchive } from './functions.js';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
-app.use(colorLog());
+app.use(colorLog);
 
 app.post('/auth/login', validateRequiredFields(['email', 'password']), validateFieldCount(2), Login);
-app.post('/auth/register', Register);
+app.post('/auth/register', validateRequiredFields(['email', 'password']), Register);
 app.post('/auth/refresh', Refresh);
-app.post('/auth/verify', Verify);
-app.post('/auth/logout', Logout);
+app.post('/auth/verify', authenticateToken, Verify);
+app.post('/auth/logout', authenticateToken, Logout);
 
 app.get('/content/all', All);
 app.get('/content/sounds/:slug', Sound);
@@ -29,4 +30,4 @@ app.post('/upload/newarchive', NewArchive);
 app.listen(3000, () => {
   console.log(`${cc.cyan}Server running on port ${cc.yellow}3000${cc.reset}`);
 });
-app.use(errorLog());
+app.use(errorLog);
