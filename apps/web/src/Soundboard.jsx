020d6ddc7bc76fd2ebab1e2dc7a0980a9fd35373
GrofRadayGedeon <mayeradam09@gmail.com>
2026-02-16 18:31:52 +0100

    Soundboard design update

--- apps/web/src/Soundboard.jsx
+++ apps/web/src/Soundboard.jsx
@@ -1,36 +1,36 @@
 import { useState } from 'react'
 import "./styles/App.css"
 
-function SoundBoard(){
-const [activeTab, setActiveTab] = useState("scenes")
+function SoundBoard() {
+    const [activeTab, setActiveTab] = useState("scenes")
 
-return(
-    <>
-    <div className='p-4'>
-    <div className='tabs-container'>
-        <button className={activeTab == "scenes" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("scenes")}>Scenes</button>
-        <button className={activeTab == "ambiences" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("ambiences")}>Ambiences</button>
-        <button className={activeTab == "oneshots" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("oneshots")}>One-Shots</button>
-    </div> 
+    return (
+        <>
+            <div className='soundboard-dsgn'>
+                <div className='soundboard-section top-section'>
+                    <div className='tabs-dsgn'>
+                        <div className='tabs-container'>
+                            <button className={activeTab == "scenes" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("scenes")}>Scenes</button>
+                            <button className={activeTab == "ambiences" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("ambiences")}>Ambiences</button>
+                            <button className={activeTab == "oneshots" ? "tab-btn active" : "tab-btn"} onClick={() => setActiveTab("oneshots")}>One-Shots</button>
+                        </div>
+                    </div>
 
-    <div className='mt-4'>
-        {activeTab == "scenes" && (
-            <p>scenes</p>
-        )}
+                    <div className='content'>
+                        {activeTab == "scenes" && <div className='ideiglenes'>Scenes</div>}
+                        {activeTab == "ambiences" && <div className='ideiglenes'>Ambiences</div>}
+                        {activeTab == "oneshots" && <div className='ideiglenes'>One-shots</div>}
+                    </div>
+                </div>
 
-        {activeTab == "ambiences" && (
-          <p>ambiences</p>
-        )}
-
-        {activeTab == "oneshots" && (
-          <p>Oneshots</p>
-        )}
-    </div>
-
-
-    </div>
-    </>
-)
+                <div className='soundboard-section bottom-section'>
+                    <div className='content-area'>
+                        <div className='ideiglenes'>Soundboard</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 
 }
 export default SoundBoard;
