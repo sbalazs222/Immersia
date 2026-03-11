ca3b6aae9fb6dc8752874e9d45ed044b906845db
Zal-n <katonazalan816@gmail.com>
2026-03-11 13:27:49 +0100

    password reset sender, todo

--- apps/backend/src/services/mail/ResetPasswordSend.js
+++ apps/backend/src/services/mail/ResetPasswordSend.js
@@ -1,1 +1,31 @@
-//TODO
+import createMailToken from "../../utils/mail/createMailToken";
+import { env } from '../../config/config.js';
+import getBlindIndex from "../../utils/emailBlindIndex";
+import sendPasswordResetMail from '../../utils/mail/sendPasswordResetMail.js';
+
+export default async function ResetPasswordSend(email, userId) {
+
+  if (!email && userId) {
+    const [emailResult] = await pool.query('SELECT AES_DECRYPT(email, ?) as email FROM users WHERE id = ?', [
+      env.DB_ENCRYPT_SECRET,
+      userId
+    ]);
+    const token = await createMailToken('password_reset', userId);
+    const link = `${env.FRONTEND_URL}/reset-password?token=${token}`;
+
+    sendPasswordResetMail(emailResult[0].email, link);
+  }
+  if (email && !userId) {
+    const blindIndex = getBlindIndex(email);
+
+    const [email] = await pool.query('SELECT AES_DECRYPT(email, ?) as email FROM users WHERE id = ?', [
+      env.DB_ENCRYPT_SECRET,
+      userId
+    ]);
+
+  }
+
+
+
+
+}
