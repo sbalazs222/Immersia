e433b8bb98906e29204443c9a2fcb45e9fc1cb6e
Zal-n <katonazalan816@gmail.com>
2026-03-12 20:34:28 +0100

    logout bug fixed

--- apps/backend/src/controllers/auth/Logout.js
+++ apps/backend/src/controllers/auth/Logout.js
@@ -1,7 +1,9 @@
 import { AuthService } from '../../services/index.js';
 
-export default async function Logout(req, res, next) {
-  await AuthService.Logout(req.user.id);
+export default async function Logout(req, res) {
+  if (req.user) {
+    await AuthService.Logout(req.user?.id);
+  }
 
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
