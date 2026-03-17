ac376b519a442daf858f4959273a2fbd0b9ed6ca
Katona Zalán <92843831+Zal-n@users.noreply.github.com>
2026-03-17 21:35:52 +0100

    Change X-Accel-Redirect path for sound files

--- apps/backend/src/controllers/content/GetSound.js
+++ apps/backend/src/controllers/content/GetSound.js
@@ -2,7 +2,7 @@ import { ContentService } from '../../services/index.js';
 
 export default async function GetSound(req, res) {
   const result = await ContentService.GetSoundBySlug(req.params.slug);
-  res.set('X-Accel-Redirect', '/internal/' + result.sound_file_path);
+  res.set('X-Accel-Redirect', '/immersia_data/' + result.sound_file_path);
   res.set('Content-Type', 'audio/mpeg');
   res.end();
 }
