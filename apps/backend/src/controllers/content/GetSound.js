a5d137b27c9eeba535c1b57b3873f6edb0746dc5
Zal-n <katonazalan816@gmail.com>
2026-03-18 20:56:46 +0100

    r3

--- apps/backend/src/controllers/content/GetSound.js
+++ apps/backend/src/controllers/content/GetSound.js
@@ -3,7 +3,7 @@ import { ContentService } from '../../services/index.js';
 export default async function GetSound(req, res) {
   const state = req.query.state;
   const result = await ContentService.GetSoundBySlug(req.params.slug);
-  res.set('X-Accel-Redirect', '/immersia_data/' + (state != '0' ? result.sound_file_path_alt : result.sound_file_path));
+  res.set('X-Accel-Redirect', '/immersia_data/' + (state != '0' ? result.sound_file_path : result.sound_file_path_alt ));
   res.set('Content-Type', 'audio/mpeg');
   res.end();
 }
