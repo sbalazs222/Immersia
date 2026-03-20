7bbdde084e480314a1b5669326481620d4dd8ed3
sbalazs222 <92843559+sbalazs222@users.noreply.github.com>
2026-03-20 08:31:20 +0100

    Fix token check logic in ConfirmEmailReceive

--- apps/backend/src/controllers/mail/ConfirmEmailReceive.js
+++ apps/backend/src/controllers/mail/ConfirmEmailReceive.js
@@ -3,7 +3,7 @@ import { ApiError } from '../../utils/apiError.js';
 
 export default async function ConfirmEmailReceive(req, res) {
   const token = req.query.token;
-  if (token) throw new ApiError(400, 'NO_TOKEN');
+  if (!token) throw new ApiError(400, 'NO_TOKEN');
   await MailService.ConfirmEmailReceive(token);
   return res.status(200).json({ message: 'SUCCESS' });
 }
