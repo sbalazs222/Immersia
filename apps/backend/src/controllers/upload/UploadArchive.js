104b05c9717d33e5460b3ce3b876a94f5e9acb58
Zal-n <katonazalan816@gmail.com>
2026-03-20 18:54:39 +0100

    input validation for mass upload, error list

--- apps/backend/src/controllers/upload/UploadArchive.js
+++ apps/backend/src/controllers/upload/UploadArchive.js
@@ -1,6 +1,10 @@
 import { UploadService } from '../../services/index.js';
 
 export default async function UploadArchive(req, res) {
-  await UploadService.UploadArchive(req.file.path);
-  return res.status(200).json({ message: 'SUCCESS' });
+  const result = await UploadService.UploadArchive(req.file.path);
+  if (result.success) {
+    return res.status(200).json({ message: 'SUCCESS' });
+  } else {
+    return res.status(400).json({ message: 'FINISHED_WITH_ERRORS', errors: result.errors });
+  }
 }
--- apps/backend/src/services/upload/UploadArchive.js
+++ apps/backend/src/services/upload/UploadArchive.js
@@ -30,18 +30,33 @@ export default async function UploadArchive(zipPath) {
       .promise();
 
     const metadata = await fse.readJSON(path.join(tempDir, 'metadata.json'));
+    if (!metadata) throw new ApiError(400, 'NO_METADATA');
 
-    const uploadPromises = metadata.map((item) => {
+    const uploadPromises = metadata.map((item, index) => {
+
+      if (!item || Object.keys(item).length === 0) return null;
+
       const slug = `${item.Title.toLowerCase()
         .trim()
         .replace(/[^a-z0-9]+/g, '-')}-${Math.random().toString(36).substring(2, 8)}`;
 
-      return limit(() => workerPool.run({ item, tempDir, slug }));
+      return limit(() => workerPool.run({ item, tempDir, slug, index }));
     });
 
-    const results = await Promise.all(uploadPromises);
+    const results = (await Promise.all(uploadPromises)).filter(Boolean); // csak a nem 'null' objektumok
+    const allErrors = results.filter(r => r.errors).map(r => r.errors);
 
-    const allCreatedFiles = results.flatMap((r) => r.files);
+    if (allErrors.length > 0) {
+      const filesToCleanup = results.filter(r => r.files).flatMap(r => r.files);
+      for (const filePath of filesToCleanup) {
+        await fse.remove(filePath).catch(() => { });
+      }
+
+      return {
+        success: false,
+        errors: allErrors
+      };
+    }
     const insertValues = results.map((r) => {
       r.dbRow.slug,
         r.dbRow.title,
@@ -49,10 +64,9 @@ export default async function UploadArchive(zipPath) {
         r.dbRow.sound_file_path,
         r.dbRow.sound_file_path_alt || null,
         r.dbRow.image_file_path
-      });
+    });
 
     try {
-      console.log()
       await pool.query(
         `INSERT INTO immersia.sounds (slug, title, type, sound_file_path, sound_file_path_alt, image_file_path) VALUES ?`,
         [insertValues]
