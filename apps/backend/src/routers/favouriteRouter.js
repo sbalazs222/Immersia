e449da37a783cff83997f6adbb0e6721775c5532
sbalazs222 <92843559+sbalazs222@users.noreply.github.com>
2026-03-22 17:43:27 +0100

    Remove authentication from favourite routes
    Removed authentication middleware from favourite routes.

--- apps/backend/src/routers/favouriteRouter.js
+++ apps/backend/src/routers/favouriteRouter.js
@@ -3,7 +3,7 @@ import { FavouriteControllers } from '../controllers/index.js';
 import { AuthenticateToken } from '../middlewares/index.js';
 const FavouriteRouter = Router();
 
-FavouriteRouter.post('/', AuthenticateToken, FavouriteControllers.AddRemoveFavourite);
-FavouriteRouter.get('/', AuthenticateToken, FavouriteControllers.GetFavourites);
+FavouriteRouter.post('/', FavouriteControllers.AddRemoveFavourite);
+FavouriteRouter.get('/', FavouriteControllers.GetFavourites);
 
 export default FavouriteRouter;
