330b9493ca652502e14d7ecb43222c36b3a9f7d3
sbalazs222 <sbalazs222@gmail.com>
2026-03-27 08:33:55 +0100

    sound type selector for admin page

--- apps/web/src/pages/Admin.jsx
+++ apps/web/src/pages/Admin.jsx
@@ -1,9 +1,50 @@
-
+import '../styles/App.css'
+import { useSoundFetch } from './soundboard/hooks/useSoundFetch'
 
 function Admin() {
+
+    const {
+        activeTab,
+        setActiveTab,
+        scenes,
+        ambiences,
+        oneshots,
+        contentAreaRef
+    } = useSoundFetch()
+
     return (
         <>
-            <h1>Admin Page</h1> 
+            <h1>Admin Page</h1>
+            <div className='soundboard-dsgn'>
+                <div className='soundboard-section'>
+                    <div className='tabs-dsgn'>
+                        <div className='tabs-container'>
+                            <button
+                                className={activeTab === 'scene' ? 'tab-btn active' : 'tab-btn'}
+                                onClick={() => setActiveTab('scene')}
+                            >
+                                Scenes
+                            </button>
+                            <button
+                                className={activeTab === 'ambience' ? 'tab-btn active' : 'tab-btn'}
+                                onClick={() => setActiveTab('ambience')}
+                            >
+                                Ambiences
+                            </button>
+                            <button
+                                className={activeTab === 'oneshot' ? 'tab-btn active' : 'tab-btn'}
+                                onClick={() => setActiveTab('oneshot')}
+                            >
+                                One-Shots
+                            </button>
+                        </div>
+                    </div>
+                    <div ref={contentAreaRef}>
+                        <p>This is the admin page. Here you can manage your soundboard content.</p>
+                        <p>{scenes.length} {ambiences.length} {oneshots.length}</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
