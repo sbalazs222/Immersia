c90e8227f42060c9f6a4766ff94233da32f79150
sbalazs222 <92843559+sbalazs222@users.noreply.github.com>
2026-03-29 11:04:57 +0200

    Fix DELETE query to handle multiple slugs

--- apps/backend/src/services/content/DeleteSound.js
+++ apps/backend/src/services/content/DeleteSound.js
@@ -11,7 +11,7 @@ export default async function DeleteSound(slugs) {
 
   const files = sounds.flatMap(item => [item.sound_file_path, item.sound_file_path_alt || undefined, item.image_file_path]).filter(item=> item != undefined)
 
-  await conn.query('DELETE FROM sounds WHERE slug = (?)', [slugs]);
+  await conn.query('DELETE FROM sounds WHERE slug IN (?)', [slugs]);
 
   for (const f of files) {
     if (f) await fse.unlink(path.join('/immersia_data', f)).catch(() => { });
